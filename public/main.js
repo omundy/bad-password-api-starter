36fbdbaf8f37c090af7848bd597749fc390588c4
Glitch (bad-password-api-starter) <none>
2024-04-22 13:18:52 +0000

    🪆📄 Updated with Glitch

--- public/main.js
+++ public/main.js
@@ -2,17 +2,7 @@
 
 // REFERENCES
 let password = document.querySelector("#password");
-let info = document.querySelector("#info");
 let button = document.querySelector("#submit");
-// INPUTS
-let common = document.querySelector("#common");
-let endearments = document.querySelector("#endearments");
-let pets = document.querySelector("#pets");
-let patterns = document.querySelector("#patterns");
-let colors = document.querySelector("#colors");
-let dates = document.querySelector("#dates");
-let cities = document.querySelector("#cities");
-let lowercase = document.querySelector("#lowercase");
 
 // EVENTS
 window.addEventListener("load", updatePassword);
@@ -20,130 +10,24 @@ document.addEventListener("submit", (e) => {
   e.preventDefault();
   updatePassword();
 });
-
-// select all checkboxes
-let checkboxes = document.querySelectorAll("input[type=checkbox]");
-// add a change event listener to each
-checkboxes.forEach((ele) => {
-  ele.addEventListener("change", handleCheck);
+// add a change event listener to each option
+document.querySelectorAll("input[type=radio]").forEach((ele) => {
+  ele.addEventListener("change", updatePassword);
 });
-
-// only allow either a common or any 3 custom parameters
-function handleCheck(e) {
-  // if common is checked,
-  if (e.target.id == "common") {
-    // then uncheck all others
-    checkboxes.forEach((ele) => {
-      if (ele.id != "common") ele.checked = false;
-    });
-  } else {
-    // otherwise, uncheck common
-    checkboxes[0].checked = false;
-    // get all checked
-    let currentlyChecked = document.querySelectorAll("input:checked");
-    if (currentlyChecked.length > 3) {
-      // uncheck current
-      e.target.checked = false;
-      displayInfo(`Don't you want ${getSynonym()} password?`);
-    }
-  }
-  updatePassword();
-}
 
 async function updatePassword() {
-  // base url for API always returns a common bad password
-  let url = "https://bad-password-api.glitch.me/api";
+//   // get the value from both groups
+//   let group1 = document.querySelector("input[name=group1]:checked");
+//   let group2 = document.querySelector("input[name=group2]:checked");
 
-  // if sending a custom request
-  if (common.checked) {
-    url += "/common";
-  } else {
-    // add the strings to a new array
-    let formData = [];
-    if (endearments.checked) formData.push("endearments");
-    if (pets.checked) formData.push("pets");
-    if (patterns.checked) formData.push("patterns");
-    if (colors.checked) formData.push("colors");
-    if (dates.checked) formData.push("dates");
-    if (cities.checked) formData.push("cities");
-    if (lowercase.checked) formData.push("lowercase");
-    // and append them to the end
-    url += "/custom?params=" + formData.join(",");
-    console.log(formData);
-  }
+//   // append options to the end
+//   let url += "/api/custom?params=" + group1.value + "," + group2.value;
 
-  // request data
-  await fetch(url)
-    .then((response) => response.json())
-    .then((json) => {
-      console.log("fetch() response", json);
-      password.value = json.password;
-    });
-}
-
-/**
- * Copy to clipboard
- */
-password.addEventListener("click", async function (event) {
-  try {
-    await navigator.clipboard.writeText(this.value);
-    displayInfo("password copied");
-  } catch (err) {
-    displayInfo("password failed to copy! " + err);
-  }
-});
-
-/**
- * Display info message
- */
-let timeout;
-function displayInfo(str) {
-  clearTimeout(timeout);
-  // if (info.innerHTML != "") return;
-  info.innerHTML = str;
-  timeout = setTimeout(function () {
-    info.innerHTML = "";
-    timeout = undefined;
-  }, 2000);
-}
-
-function getSynonym() {
-  let str = randomFromArray(badSynonyms);
-  if (/^[aeiou]$/i.test(str.charAt(0))) str = "an " + str;
-  else str = "a " + str;
-  return str;
-}
-let badSynonyms = `bad
-        atrocious
-        awful
-        cheap
-        crummy
-        dreadful
-        regrettable
-        dreadful
-        unusable
-        lousy
-        poor
-        sad
-        unacceptable
-        garbage
-        imperfect
-        inferior
-        abominable
-        careless
-        crappy
-        defective
-        deficient
-        erroneous
-        fallacious
-        faulty
-        inadequate
-        slipshod
-        substandard
-        unsatisfactory
-        `
-  .trim()
-  .split(/\W+/);
-function randomFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+//   // request data
+//   await fetch(url)
+//     .then((response) => response.json())
+//     .then((json) => {
+//       console.log("fetch() response", json);
+//       password.value = json.message;
+//     });
 }
--- routes.js
+++ routes.js
@@ -1,1 +1,51 @@
 // route.js - Add endpoints to the API
+
+// import data for the API
+import { data, functions } from "./data.js";
+
+const routes = async (server, options) => {
+  
+  // //1
+  // server.get("/api", async function (request, reply) {
+  //   reply.send({ message: 123 });
+  // });
+  // //2
+  // server.get("/api/common", async function (request, reply) {
+  //   reply.send({ message: functions.randomFromArray(data.common) });
+  // });
+  // //3
+  // server.get("/api/custom", async function (request, reply) {
+  //   console.log(request.query.params);
+  //   reply.send({ messageeturnPassword(request.query.params) });
+  // });
+};
+export default routes;
+
+function returnPassword(params) {
+  let str = "";
+
+  // group 1
+  if (params.includes("endearments")) {
+    str += functions.randomFromArray(data.endearments);
+  }
+  if (params.includes("pets")) {
+    str += functions.randomFromArray(data.pets);
+  }
+  if (params.includes("cities")) {
+    str += functions.randomFromArray(data.cities);
+  }
+  // group 2
+  if (params.includes("colors")) {
+    str += functions.randomFromArray(data.colors);
+  }
+  if (params.includes("dates")) {
+    str += functions.randomYear();
+  }
+  if (params.includes("patterns")) {
+    str += functions.randomFromArray(data.patterns);
+  }
+
+  return str;
+}
+
+
