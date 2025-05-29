ca674f5663edacf5e5a774d0c531632b62d65818
Owen Mundy <omundy@gmail.com>
2025-05-29 14:01:55 -0400

    Random changes

--- public/main.js
+++ public/main.js
@@ -4,15 +4,6 @@
 let password = document.querySelector("#password");
 let info = document.querySelector("#info");
 let button = document.querySelector("#submit");
-// INPUTS
-let common = document.querySelector("#common");
-let endearments = document.querySelector("#endearments");
-let pets = document.querySelector("#pets");
-let patterns = document.querySelector("#patterns");
-let colors = document.querySelector("#colors");
-let dates = document.querySelector("#dates");
-let cities = document.querySelector("#cities");
-let lowercase = document.querySelector("#lowercase");
 
 // EVENTS
 window.addEventListener("load", updatePassword);
@@ -21,129 +12,15 @@ document.addEventListener("submit", (e) => {
   updatePassword();
 });
 
-// select all checkboxes
-let checkboxes = document.querySelectorAll("input[type=checkbox]");
-// add a change event listener to each
-checkboxes.forEach((ele) => {
-  ele.addEventListener("change", handleCheck);
-});
-
-// only allow either a common or any 3 custom parameters
-function handleCheck(e) {
-  // if common is checked,
-  if (e.target.id == "common") {
-    // then uncheck all others
-    checkboxes.forEach((ele) => {
-      if (ele.id != "common") ele.checked = false;
-    });
-  } else {
-    // otherwise, uncheck common
-    checkboxes[0].checked = false;
-    // get all checked
-    let currentlyChecked = document.querySelectorAll("input:checked");
-    if (currentlyChecked.length > 3) {
-      // uncheck current
-      e.target.checked = false;
-      displayInfo(`Don't you want ${getSynonym()} password?`);
-    }
-  }
-  updatePassword();
-}
-
 async function updatePassword() {
   // base url for API always returns a common bad password
-  let url = "https://bad-password-api.glitch.me/api";
-
-  // if sending a custom request
-  if (common.checked) {
-    url += "/common";
-  } else {
-    // add the strings to a new array
-    let formData = [];
-    if (endearments.checked) formData.push("endearments");
-    if (pets.checked) formData.push("pets");
-    if (patterns.checked) formData.push("patterns");
-    if (colors.checked) formData.push("colors");
-    if (dates.checked) formData.push("dates");
-    if (cities.checked) formData.push("cities");
-    if (lowercase.checked) formData.push("lowercase");
-    // and append them to the end
-    url += "/custom?params=" + formData.join(",");
-    console.log(formData);
-  }
+  let url = "./api";
 
   // request data
   await fetch(url)
     .then((response) => response.json())
     .then((json) => {
       console.log("fetch() response", json);
-      password.value = json.password;
+      password.value = json.message;
     });
-}
-
-/**
- * Copy to clipboard
- */
-password.addEventListener("click", async function (event) {
-  try {
-    await navigator.clipboard.writeText(this.value);
-    displayInfo("password copied");
-  } catch (err) {
-    displayInfo("password failed to copy! " + err);
-  }
-});
-
-/**
- * Display info message
- */
-let timeout;
-function displayInfo(str) {
-  clearTimeout(timeout);
-  // if (info.innerHTML != "") return;
-  info.innerHTML = str;
-  timeout = setTimeout(function () {
-    info.innerHTML = "";
-    timeout = undefined;
-  }, 2000);
-}
-
-function getSynonym() {
-  let str = randomFromArray(badSynonyms);
-  if (/^[aeiou]$/i.test(str.charAt(0))) str = "an " + str;
-  else str = "a " + str;
-  return str;
-}
-let badSynonyms = `bad
-        atrocious
-        awful
-        cheap
-        crummy
-        dreadful
-        regrettable
-        dreadful
-        unusable
-        lousy
-        poor
-        sad
-        unacceptable
-        garbage
-        imperfect
-        inferior
-        abominable
-        careless
-        crappy
-        defective
-        deficient
-        erroneous
-        fallacious
-        faulty
-        inadequate
-        slipshod
-        substandard
-        unsatisfactory
-        `
-  .trim()
-  .split(/\W+/);
-function randomFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
 }
--- routes.js
+++ routes.js
@@ -1,1 +1,8 @@
 // route.js - Add endpoints to the API
+
+const routes = async (server, options) => {
+	server.get("/api", function (request, reply) {
+		reply.send({ message: 123 });
+	});
+};
+export default routes;
